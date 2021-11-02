f4a919c1ea5c5798a3aeb4e2336a61b07b00ce60
ag129 <62814033+ag129@users.noreply.github.com>
2021-11-02 16:32:25 -0400

    Update g5.py
    First Scoring Function

--- suitors/g5.py
+++ suitors/g5.py
@@ -1,8 +1,12 @@
+from collections import Counter
 from typing import Dict
 
+from constants import MAX_BOUQUET_SIZE
 from flowers import Bouquet, Flower, FlowerSizes, FlowerColors, FlowerTypes
 from suitors.base import BaseSuitor
-
+import random
+import numpy as np
+from utils import flatten_counter
 
 class Suitor(BaseSuitor):
     def __init__(self, days: int, num_suitors: int, suitor_id: int):
@@ -12,6 +16,25 @@ class Suitor(BaseSuitor):
         :param suitor_id: unique id of your suitor in range(num_suitors)
         """
         super().__init__(days, num_suitors, suitor_id, name='g5')
+        bad_color_num = random.randint(0,len(FlowerColors)-1)
+        self.bad_color_enum = FlowerColors(bad_color_num)
+
+    #from random_suitor
+    '''
+    def _prepare_bouquet(self, remaining_flowers, recipient_id):
+        num_remaining = sum(remaining_flowers.values())
+        size = int(np.random.randint(0, min(MAX_BOUQUET_SIZE, num_remaining) + 1))
+        if size > 0:
+            chosen_flowers = np.random.choice(flatten_counter(remaining_flowers), size=(size, ), replace=False)
+            chosen_flower_counts = dict(Counter(chosen_flowers))
+            for k, v in chosen_flower_counts.items():
+                remaining_flowers[k] -= v
+                assert remaining_flowers[k] >= 0
+        else:
+            chosen_flower_counts = dict()
+        chosen_bouquet = Bouquet(chosen_flower_counts)
+        return self.suitor_id, recipient_id, chosen_bouquet
+    '''
 
     def prepare_bouquets(self, flower_counts: Dict[Flower, int]):
         """
@@ -25,40 +48,65 @@ class Suitor(BaseSuitor):
         all_ids = np.arange(self.num_suitors)
         recipient_ids = all_ids[all_ids != self.suitor_id]
         """
-        pass
+        '''
+        #from random_suitor
+        all_ids = np.arange(self.num_suitors)
+        recipient_ids = all_ids[all_ids != self.suitor_id]
+        remaining_flowers = flower_counts.copy()
+        return list(map(lambda recipient_id: self._prepare_bouquet(remaining_flowers, recipient_id), recipient_ids))
+        '''
+        all_ids = np.arange(self.num_suitors)
+        recipient_ids = all_ids[all_ids != self.suitor_id]
+        l = []
+        for r in recipient_ids:
+            l.append((self.suitor_id, r, Bouquet({})))
+        return l
 
     def zero_score_bouquet(self):
         """
         :return: a Bouquet for which your scoring function will return 0
         """
-        pass
+        f = Flower(FlowerSizes.Small, self.bad_color_enum, FlowerTypes.Rose)
+        d = {f: 1}
+        return Bouquet(d)
 
     def one_score_bouquet(self):
         """
         :return: a Bouquet for which your scoring function will return 1
         """
-        pass
+        good_color_enum = None
+        for v in FlowerColors:
+            if v != self.bad_color_enum:
+                good_color_enum = v
+                break
+        f = Flower(FlowerSizes.Small, good_color_enum, FlowerTypes.Rose)
+        d = {f: 1}
+        return Bouquet(d)
 
     def score_types(self, types: Dict[FlowerTypes, int]):
         """
         :param types: dictionary of flower types and their associated counts in the bouquet
         :return: A score representing preference of the flower types in the bouquet
         """
-        pass
+        return 0.0
 
     def score_colors(self, colors: Dict[FlowerColors, int]):
         """
         :param colors: dictionary of flower colors and their associated counts in the bouquet
         :return: A score representing preference of the flower colors in the bouquet
         """
-        pass
+        for c, v in colors.items():
+            if c == self.bad_color_enum and v != 0:
+                return 0
+
+        return 1.0
 
     def score_sizes(self, sizes: Dict[FlowerSizes, int]):
         """
         :param sizes: dictionary of flower sizes and their associated counts in the bouquet
         :return: A score representing preference of the flower sizes in the bouquet
         """
-        pass
+        return 0.0
 
     def receive_feedback(self, feedback):
         """
@@ -66,3 +114,4 @@ class Suitor(BaseSuitor):
         :return: nothing
         """
         self.feedback.append(feedback)
+

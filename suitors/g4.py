053fb4559998e18ce3510942a5c5ef01a47d37a7
mwinitch <michaelwin26@gmail.com>
2021-11-02 17:26:21 -0400

    Updated group code with new flower choosing and scoring functions

--- suitors/g4.py
+++ suitors/g4.py
@@ -2,6 +2,9 @@ from typing import Dict
 
 from flowers import Bouquet, Flower, FlowerSizes, FlowerColors, FlowerTypes
 from suitors.base import BaseSuitor
+from random import shuffle
+import numpy as np
+from utils import flatten_counter
 
 
 class Suitor(BaseSuitor):
@@ -12,6 +15,29 @@ class Suitor(BaseSuitor):
         :param suitor_id: unique id of your suitor in range(num_suitors)
         """
         super().__init__(days, num_suitors, suitor_id, name='g4')
+        self.size_mapping = self.generate_map(FlowerSizes)
+        self.color_mapping = self.generate_map(FlowerColors)
+        self.type_mapping = self.generate_map(FlowerTypes)
+        self.best_arrangement = self.best_bouquet()
+
+    def generate_map(self, flower_enum):
+        sizes = [n.value for n in flower_enum]
+        shuffle(sizes)
+        mapping = {}
+        for idx, name in enumerate(flower_enum):
+            mapping[name] = sizes[idx]
+        return mapping
+    
+    def best_bouquet(self):
+        best_size = (sorted([(key, value) for key, value in self.size_mapping.items()], key=lambda x: x[1]))[-1][0]
+        best_color = (sorted([(key, value) for key, value in self.color_mapping.items()], key=lambda x: x[1]))[-1][0]
+        best_type = (sorted([(key, value) for key, value in self.type_mapping.items()], key=lambda x: x[1]))[-1][0]
+        best_flower = Flower(
+            size=best_size,
+            color=best_color,
+            type=best_type
+        )
+        return Bouquet({best_flower: 1})
 
     def prepare_bouquets(self, flower_counts: Dict[Flower, int]):
         """
@@ -31,34 +57,46 @@ class Suitor(BaseSuitor):
         """
         :return: a Bouquet for which your scoring function will return 0
         """
-        pass
+        return Bouquet({})
 
     def one_score_bouquet(self):
         """
         :return: a Bouquet for which your scoring function will return 1
         """
-        pass
+        return self.best_arrangement
 
     def score_types(self, types: Dict[FlowerTypes, int]):
         """
         :param types: dictionary of flower types and their associated counts in the bouquet
         :return: A score representing preference of the flower types in the bouquet
         """
-        pass
+        if len(types) == 0:
+            return 0.0
+
+        avg_types = float(np.mean([self.type_mapping[x] for x in flatten_counter(types)]))
+        return avg_types / (3 * (len(FlowerTypes) - 1))
 
     def score_colors(self, colors: Dict[FlowerColors, int]):
         """
         :param colors: dictionary of flower colors and their associated counts in the bouquet
         :return: A score representing preference of the flower colors in the bouquet
         """
-        pass
+        if len(colors) == 0:
+            return 0.0
+
+        avg_types = float(np.mean([self.type_mapping[x] for x in flatten_counter(colors)]))
+        return avg_types / (3 * (len(FlowerColors) - 1))
 
     def score_sizes(self, sizes: Dict[FlowerSizes, int]):
         """
         :param sizes: dictionary of flower sizes and their associated counts in the bouquet
         :return: A score representing preference of the flower sizes in the bouquet
         """
-        pass
+        if len(sizes) == 0:
+            return 0.0
+
+        avg_types = float(np.mean([self.type_mapping[x] for x in flatten_counter(sizes)]))
+        return avg_types / (3 * (len(FlowerSizes) - 1))
 
     def receive_feedback(self, feedback):
         """

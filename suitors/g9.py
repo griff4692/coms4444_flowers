1a8436eeae6af4f35208086d584b98fd714e9f16
silvia <silvia@metrika.co>
2021-11-03 01:14:38 -0400

    initial receive feeback & create bouquets strategy

--- suitors/g9.py
+++ suitors/g9.py
@@ -1,8 +1,14 @@
 from typing import Dict
+from collections import Counter
 
+import numpy as np
+import random
+
+from constants import MAX_BOUQUET_SIZE
 from flowers import Bouquet, Flower, FlowerSizes, FlowerColors, FlowerTypes
 from suitors.base import BaseSuitor
-
+from suitors import random_suitor
+from utils import flatten_counter
 
 class Suitor(BaseSuitor):
     def __init__(self, days: int, num_suitors: int, suitor_id: int):
@@ -12,6 +18,24 @@ class Suitor(BaseSuitor):
         :param suitor_id: unique id of your suitor in range(num_suitors)
         """
         super().__init__(days, num_suitors, suitor_id, name='g9')
+        self.bouquets = {} # dictionary with the bouquet we gave to each player in a given round
+                           # create similar round that stores all bouquets along with their scores for every round
+        self.random_suiton = random_suitor.RandomSuitor(days, num_suitors, suitor_id)
+
+    def _prepare_bouquet(self, remaining_flowers, recipient_id):
+        num_remaining = sum(remaining_flowers.values())
+        size = int(np.random.randint(0, min(MAX_BOUQUET_SIZE, num_remaining) + 1))
+        if size > 0:
+            chosen_flowers = np.random.choice(flatten_counter(remaining_flowers), size=(size, ), replace=False)
+            chosen_flower_counts = dict(Counter(chosen_flowers))
+            for k, v in chosen_flower_counts.items():
+                remaining_flowers[k] -= v
+                assert remaining_flowers[k] >= 0
+        else:
+            chosen_flower_counts = dict()
+        chosen_bouquet = Bouquet(chosen_flower_counts)
+        self.bouquets[recipient_id] = chosen_bouquet # store the bouquet we gave to each player in this round 
+        return self.suitor_id, recipient_id, chosen_bouquet
 
     def prepare_bouquets(self, flower_counts: Dict[Flower, int]):
         """
@@ -19,13 +43,20 @@ class Suitor(BaseSuitor):
         :return: list of tuples of (self.suitor_id, recipient_id, chosen_bouquet)
         the list should be of length len(self.num_suitors) - 1 because you should give a bouquet to everyone
          but yourself
-
         To get the list of suitor ids not including yourself, use the following snippet:
-
         all_ids = np.arange(self.num_suitors)
         recipient_ids = all_ids[all_ids != self.suitor_id]
         """
-        pass
+        all_ids = np.arange(self.num_suitors)
+        recipient_ids = all_ids[all_ids != self.suitor_id]
+        remaining_flowers = flower_counts.copy()
+        if len(self.feedback) == 0: # first round, so we don't have feedback
+            return list(map(lambda recipient_id: self._prepare_bouquet(remaining_flowers, recipient_id), recipient_ids))
+        else:
+            prev_round_feedback = self.feedback[len(self.feedback)-1]
+            recipient_ids = list(zip(*prev_round_feedback))[1] # sort recipiernt_ids by final score to prioritize players
+            return list(map(lambda recipient_id: self._prepare_bouquet(remaining_flowers, recipient_id), recipient_ids))
+
 
     def zero_score_bouquet(self):
         """
@@ -65,4 +96,15 @@ class Suitor(BaseSuitor):
         :param feedback:
         :return: nothing
         """
-        self.feedback.append(feedback)
+        print(self.suitor_id)
+        final_scores_tuples = [] # a list of tuples (final_score, suitor_num, bouquet)
+        for suitor_num, (rank, score) in enumerate(feedback):
+            if suitor_num != self.suitor_id: # we shouldn't add ourselves to the list of players for whom we will create a bouquet
+                # TODO: update final_score claculation to 
+                # 1) give more weight to ranking
+                # 2) take into consideration the number of people who got the same ranking
+                # maybe use a final_score =w_1*rank + w_2*score function
+                final_score = score/rank
+                final_scores_tuples.append((final_score, suitor_num, self.bouquets[suitor_num]))
+        self.feedback.append(sorted(final_scores_tuples, reverse=True))
+

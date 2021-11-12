595d648eade033045f065b22b3bf1a33109541fe
vaibhavbagri <32847060+vaibhavbagri@users.noreply.github.com>
2021-11-11 19:39:16 -0500

    Controlled experiment and bug fix in best bouquet
    Controlled experiment has been implemented, but have just kept it commented so that we can discuss the parameters.

--- suitors/g8.py
+++ suitors/g8.py
@@ -25,10 +25,17 @@ class Suitor(BaseSuitor):
         # pick one attribute to null
         self.null = random.randint(0,2) # 0=type, 1=color, 2=size
         self.given = {} # dictionary to save the bouquets we gave + their scores
-        for suitor in range(0,num_suitors):
-            if suitor!= suitor_id:
-                self.given[suitor] = [] # list for each suitor, will be list of lists with [bouquet, score]
+        all_ids = np.arange(self.num_suitors)
+        self.recipient_ids = all_ids[all_ids != self.suitor_id]
+        for suitor in self.recipient_ids:
+            self.given[suitor] = [] # list for each suitor, will be list of lists with [bouquet, score]
         self.days_left = days
+        self.day_number = 0
+        self.prefs = {}
+        self.initialize_preferences()
+        self.controlled_strat = {}
+        self.initalize_controlled_strat()
+        self.suitors_to_test = list(self.recipient_ids)
 
 
     def _prepare_bouquet(self, remaining_flowers, recipient_id):
@@ -45,31 +52,28 @@ class Suitor(BaseSuitor):
         chosen_bouquet = Bouquet(chosen_flower_counts)
         return self.suitor_id, recipient_id, chosen_bouquet
 
-    def best_bouquets(self, remaining_flowers, suitors, scores_per_player):
-        num_remaining = sum(remaining_flowers.values())
-        size = int(np.random.randint(0, min(MAX_BOUQUET_SIZE, num_remaining) + 1))
+    def best_bouquets(self, remaining_flowers, scores_per_player):
+        flowers_in_market = flatten_counter(remaining_flowers)
         chosen_bouquets = {}
         suitor_bouquet_counts = {}
-        for suitor in range(0, suitors):
-            if suitor!= self.suitor_id:
-                chosen_bouquets[suitor] = {}
-                suitor_bouquet_counts[suitor] = 0
+        for suitor in self.recipient_ids:
+            chosen_bouquets[suitor] = {}
+            suitor_bouquet_counts[suitor] = 0
 
-        for flower in remaining_flowers:
+        for flower in flowers_in_market:
             color = flower.color
             type = flower.type
             size = flower.size
             max_score=0
             suitor_getting_flower=0
-            for suitor in range(0, suitors):
-                if suitor != self.suitor_id:
-                    color_score = scores_per_player[suitor]["color"][color.value]
-                    type_score = scores_per_player[suitor]["type"][type.value]
-                    size_score = scores_per_player[suitor]["size"][size.value]
-                    total_score = color_score+type_score+size_score
-                    if total_score>max_score and suitor_bouquet_counts[suitor]<12:
-                        max_score=total_score
-                        suitor_getting_flower=suitor
+            for suitor in self.recipient_ids:
+                color_score = scores_per_player[suitor]["color"][color.value]
+                type_score = scores_per_player[suitor]["type"][type.value]
+                size_score = scores_per_player[suitor]["size"][size.value]
+                total_score = color_score+type_score+size_score
+                if total_score>max_score and suitor_bouquet_counts[suitor]<6:
+                    max_score=total_score
+                    suitor_getting_flower=suitor
             if flower in chosen_bouquets[suitor_getting_flower]:
                 chosen_bouquets[suitor_getting_flower][flower] = chosen_bouquets[suitor_getting_flower][flower]+1
             else:
@@ -78,8 +82,30 @@ class Suitor(BaseSuitor):
 
         return chosen_bouquets
 
-
-
+    def convert_prefs_to_scores_per_player(self):
+        scores_by_attribute = {}
+        colors = [c.name for c in FlowerColors]
+        sizes = [s.name for s in FlowerSizes]
+        types = [t.name for t in FlowerTypes]
+        for suitor, prefs in self.prefs.items():
+            preferences = {}
+            preferences["color"] = [0]*6
+            preferences["type"] = [0]*4
+            preferences["size"] = [0]*3
+            for param, val in prefs.items():
+                if param in colors:
+                    i = colors.index(param)
+                    preferences["color"][i] = val
+                    continue
+                if param in types:
+                    i = types.index(param)
+                    preferences["type"][i] = val
+                    continue
+                if param in sizes:
+                    i = sizes.index(param)
+                    preferences["size"][i] = val
+            scores_by_attribute[suitor] = preferences
+        return scores_by_attribute
 
 
     def scores_per_player(self, given):
@@ -139,6 +165,22 @@ class Suitor(BaseSuitor):
 
         To get the list of suitor ids not including yourself, use the following snippet:"""
 
+        self.day_number += 1
+        # if self.day_number == self.days:
+        #     scores_per_player = self.convert_prefs_to_scores_per_player()
+        #     # scores_per_player is already excluding us, and is in order of the other suitors (index = suitor)
+        #     chosen_bouquets = self.best_bouquets(flower_counts.copy(), scores_per_player)
+        #     bouquets = []
+        #     for suitor in chosen_bouquets:
+        #         bouquets.append((self.suitor_id, suitor, Bouquet(chosen_bouquets[suitor])))
+        #     print(str(self.suitor_id) + str(self.size_weights) +str(self.type_weights) + str(self.color_weights))
+        #     return bouquets
+        # if self.suitors_to_test:
+        #     return self.use_controlled_strategy(flower_counts.copy())
+        # return {}
+
+
+
         # this loop inputs the score for each bouquet we gave last round
         if len(self.feedback)!=0:
             this_rounds_feedback = self.feedback[-1]
@@ -158,7 +200,7 @@ class Suitor(BaseSuitor):
             # list of (self.suitor_id, recipient_id, chosen_bouquet)
             scores_per_player = self.scores_per_player(self.given) # list of dictionaries {number: [number preferences], color: [color preferences], etc.}
             # scores_per_player is already excluding us, and is in order of the other suitors (index = suitor)
-            chosen_bouquets = self.best_bouquets(remaining_flowers, self.num_suitors, scores_per_player)
+            chosen_bouquets = self.best_bouquets(remaining_flowers, scores_per_player)
             bouquets = []
             for suitor in chosen_bouquets:
                 bouquets.append((self.suitor_id, suitor, Bouquet(chosen_bouquets[suitor])))
@@ -286,3 +328,125 @@ class Suitor(BaseSuitor):
         :return: nothing
         """
         self.feedback.append(feedback)
+
+    def initialize_preferences(self):
+        for suitor in self.recipient_ids:
+            self.prefs[suitor] = {
+                "Small" : 0,
+                "Medium" : 0,
+                "Large" : 0,
+                "White" : 0,
+                "Yellow" : 0,
+                "Red" : 0,
+                "Purple" : 0,
+                "Orange" : 0,
+                "Blue" : 0,
+                "Rose" : 0,
+                "Chrysanthemum" : 0,
+                "Tulip" : 0,
+                "Begonia" : 0,
+                "None" : 0
+            }
+
+    def initalize_controlled_strat(self):
+        for suitor in self.recipient_ids:
+            self.controlled_strat[suitor] = {
+                "colors" : [c for c in FlowerColors],
+                "types" : [t for t in FlowerTypes],
+                "sizes" : [s for s in FlowerSizes],
+                "const_params_color" : [],
+                "const_params_type": [],
+                "const_params_size": [],
+                "tested_param" : None
+            }
+
+    def start_controlled_color(self, suitor, flower):
+        self.controlled_strat[suitor]["colors"].remove(flower.color)
+        self.controlled_strat[suitor]["const_params_color"] = [flower.size, flower.type]
+        self.controlled_strat[suitor]["tested_param"] = flower.color.name
+
+    def start_controlled_type(self, suitor, flower):
+        self.controlled_strat[suitor]["types"].remove(flower.type)
+        self.controlled_strat[suitor]["const_params_type"] = [flower.color, flower.size]
+        self.controlled_strat[suitor]["tested_param"] = flower.type.name
+
+    def start_controlled_size(self, suitor, flower):
+        self.controlled_strat[suitor]["sizes"].remove(flower.size)
+        self.controlled_strat[suitor]["const_params_size"] = [flower.color, flower.type]
+        self.controlled_strat[suitor]["tested_param"] = flower.size.name
+
+    def update_preferences(self):
+        if self.day_number > 1:
+            latest_feedback = self.feedback[-1]
+            for suitor in self.suitors_to_test:
+                self.prefs[suitor][self.controlled_strat[suitor]["tested_param"]] = latest_feedback[suitor][1]
+
+    def get_flowers_to_test(self, flowers_in_market, suitor):
+        all_tested_flag = True
+        for color in self.controlled_strat[suitor]["colors"]:
+            all_tested_flag = False
+            reqd_flower = Flower(
+                size=self.controlled_strat[suitor]["const_params_color"][0],
+                color=color,
+                type=self.controlled_strat[suitor]["const_params_color"][1]
+            )
+            if reqd_flower in flowers_in_market:
+                self.controlled_strat[suitor]["colors"].remove(color)
+                self.controlled_strat[suitor]["tested_param"] = color.name
+                return reqd_flower, all_tested_flag
+
+        for type in self.controlled_strat[suitor]["types"]:
+            all_tested_flag = False
+            reqd_flower = Flower(
+                size=self.controlled_strat[suitor]["const_params_type"][1],
+                color=self.controlled_strat[suitor]["const_params_type"][0],
+                type=type
+            )
+            if reqd_flower in flowers_in_market:
+                self.controlled_strat[suitor]["types"].remove(type)
+                self.controlled_strat[suitor]["tested_param"] = type.name
+                return reqd_flower, all_tested_flag
+
+        for size in self.controlled_strat[suitor]["sizes"]:
+            all_tested_flag = False
+            reqd_flower = Flower(
+                size=size,
+                color=self.controlled_strat[suitor]["const_params_size"][0],
+                type=self.controlled_strat[suitor]["const_params_size"][1]
+            )
+            if reqd_flower in flowers_in_market:
+                self.controlled_strat[suitor]["sizes"].remove(size)
+                self.controlled_strat[suitor]["tested_param"] = size.name
+                return reqd_flower, all_tested_flag
+        self.controlled_strat[suitor]["tested_param"] = "None"
+        return None, all_tested_flag
+
+    def use_controlled_strategy(self, remaining_flowers):
+        chosen_bouquets = {}
+        flowers_in_market = flatten_counter(remaining_flowers)
+        self.update_preferences()
+        for suitor in list(self.suitors_to_test):
+            all_tested = False
+            if self.day_number == 1:
+                chosen_flower = np.random.choice(list(set(flowers_in_market)))
+                self.start_controlled_color(suitor, chosen_flower)
+            elif self.day_number == 2:
+                chosen_flower = np.random.choice(list(set(flowers_in_market)))
+                self.start_controlled_type(suitor, chosen_flower)
+            elif self.day_number == 3:
+                chosen_flower = np.random.choice(list(set(flowers_in_market)))
+                self.start_controlled_size(suitor, chosen_flower)
+            else:
+                chosen_flower, all_tested = self.get_flowers_to_test(flowers_in_market, suitor)
+
+            if chosen_flower is not None:
+                chosen_bouquets[suitor] = chosen_flower
+                flowers_in_market.remove(chosen_flower)
+            if all_tested:
+                self.suitors_to_test.remove(suitor)
+
+        bouquets = []
+        for k, v in chosen_bouquets.items():
+            bouquets.append((self.suitor_id, k, Bouquet({v: 1})))
+
+        return bouquets

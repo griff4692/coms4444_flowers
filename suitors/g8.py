dc6a7fcd08ba415392c6a02d6990b65165e03d1e
samrmayers <59097628+samrmayers@users.noreply.github.com>
2021-11-14 11:24:44 -0500

    changed max bouquet to 7. Different weights days=1

--- suitors/g8.py
+++ suitors/g8.py
@@ -16,14 +16,19 @@ class Suitor(BaseSuitor):
         :param suitor_id: unique id of your suitor in range(num_suitors)
         """
         super().__init__(days, num_suitors, suitor_id, name='g8')
-        self.type_weights = [1/(self.days**2), 1/(self.days), 2/(self.days), 1]
-        self.color_weights = [1/(self.days**2), 1/(self.days**2), 1/(self.days), 1/(self.days), 2/(self.days), 1]
-        self.size_weights = [1/(self.days**2), 1/(self.days), 1]
-        random.shuffle(self.type_weights)
-        random.shuffle(self.color_weights)
-        random.shuffle(self.size_weights)
-        # pick one attribute to null
-        self.null = random.randint(0,2) # 0=type, 1=color, 2=size
+        if self.days > 1:
+            self.type_weights = [1/(self.days**2), 1/(self.days), 2/(self.days), 1]
+            self.color_weights = [1/(self.days**2), 1/(self.days**2), 1/(self.days), 1/(self.days), 2/(self.days), 1]
+            self.size_weights = [1/(self.days**2), 1/(self.days), 1]
+            random.shuffle(self.type_weights)
+            random.shuffle(self.color_weights)
+            random.shuffle(self.size_weights)
+            # pick one attribute to null
+            self.null = random.randint(0,2) # 0=type, 1=color, 2=size
+        elif self.days==1:
+            self.color_weights = [1 / (self.num_suitors ** 2), 1 / (self.num_suitors ** 2), 1 / (self.num_suitors), 1 / (self.num_suitors),
+                                  2 / (self.num_suitors), 1]
+            self.null = 3 # so its ignored by scoring methods
         self.given = {} # dictionary to save the bouquets we gave + their scores
         all_ids = np.arange(self.num_suitors)
         self.recipient_ids = all_ids[all_ids != self.suitor_id]
@@ -71,7 +76,7 @@ class Suitor(BaseSuitor):
                 type_score = scores_per_player[suitor]["type"][type.value]
                 size_score = scores_per_player[suitor]["size"][size.value]
                 total_score = color_score+type_score+size_score
-                if total_score>max_score and suitor_bouquet_counts[suitor]<6:
+                if total_score>max_score and suitor_bouquet_counts[suitor]<7:
                     max_score=total_score
                     suitor_getting_flower=suitor
             if flower in chosen_bouquets[suitor_getting_flower]:
@@ -106,7 +111,6 @@ class Suitor(BaseSuitor):
                     preferences["size"][i] = val
             scores_by_attribute[suitor] = preferences
         return scores_by_attribute
-
 
     def scores_per_player(self, given):
         scores_by_attribute = {} # will be a dictionary of dictionaries, one per suitor, dictionaries contain info on scores per attribute
@@ -153,8 +157,6 @@ class Suitor(BaseSuitor):
             scores_by_attribute[suitor]=preferences
 
         return scores_by_attribute
-
-
 
     def prepare_bouquets(self, flower_counts: Dict[Flower, int]):
 
@@ -218,8 +220,6 @@ class Suitor(BaseSuitor):
 
         return bouquets
 
-
-
     def zero_score_bouquet(self):
         """
         :return: a Bouquet for which your scoring function will return 0
@@ -242,7 +242,7 @@ class Suitor(BaseSuitor):
         :param types: dictionary of flower types and their associated counts in the bouquet
         :return: A score representing preference of the flower types in the bouquet
         """
-        if self.null == 0:
+        if self.null == 0 or self.days == 1:
             return 0
 
         score = 0
@@ -260,7 +260,6 @@ class Suitor(BaseSuitor):
 
         # multiply by .4 since each type, sixe, color is .4 weight but one is dropped + .2 number
         return score*.4
-
 
     def score_colors(self, colors: Dict[FlowerColors, int]):
         """
@@ -283,6 +282,9 @@ class Suitor(BaseSuitor):
         if total != 0:
             score = score / total
 
+        # color only one if days = 1
+        if self.days == 1:
+            return score
         # multiply by .4 since each type, sixe, color is .4 weight but one is dropped + .2 number
         return score * .4
 
@@ -313,11 +315,11 @@ class Suitor(BaseSuitor):
         # multiply by .4 since each type, sixe, color is .4 weight but one is dropped + .2 number
         score = score * .4
 
-        if self.null == 2:
+        if self.null == 2 or self.days==1:
             score = 0
 
         # count number of flowers for the last .25
-        weights_number = [0, .1, .2, .32, .48, .56, .72, .8, .88, 1, 1, 1, 1]
+        weights_number = [0, .05, .18, .32, .48, .72, .85, 1, 1, 1, 1, 1, 1]
         score_count = weights_number[total]*.2
 
         return score+score_count
